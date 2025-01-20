d4f6218cb3f630103d8095ebc4aefbfa72dcdcb1
rds-mimosa <alexsander.raphaeldisanto@gmail.com>
2025-01-20 15:15:14 -0300

    1

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ function loadHTML(id, url) {
 }
 
 document.addEventListener("DOMContentLoaded", function() {
-    loadHTML('head', 'head/head.html'); 
+    //loadHTML('head', 'head/head.html'); 
     loadHTML('header', 'header/header.html');
     loadHTML('content', 'main/main-content.html');
     loadHTML('footer', 'footer/footer.html');
